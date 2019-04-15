196473d89346510370ce4a9e53e9b6e80d6fea52
rama.gottfried <rama.gottfried@gmail.com>
2019-04-15 22:51:36 +0200

    removed autosync, added do_sync, called on init

--- code/node/lib/drawsocket-client.js
+++ code/node/lib/drawsocket-client.js
@@ -389,8 +389,10 @@ var drawsocket = (function(){
 
           let fix_position = (timestamp) => {
             let bb = el.node().getBBox();
-            if( bb.width === 0 && retries-- > 0)
+            if( bb.width === 0 && retries-- > 0){
+//              console.log('retrying');
               window.requestAnimationFrame(fix_position);
+            }
             else
             {
               TweenMax.set(el.node(), {x: oldx - bb.x, y: oldy - bb.y, width: bb.width, height: bb.height} );
@@ -1699,6 +1701,10 @@ var drawsocket = (function(){
             else
               disableMultitouch();
         break;
+        case "do_sync":
+          do_sync();
+        break;
+
         default:
             console.log("unrouted command key:", key, objValue );
         break;
@@ -1853,6 +1859,13 @@ var drawsocket = (function(){
 
   }
 
+  function do_sync()
+  {
+    setTimeout(function () {
+      ts.sync().catch(err => console.loig('timesync err', err));
+    }, 100);
+  }
+
   function _SocketPort_()
   {
 
@@ -1970,7 +1983,7 @@ var drawsocket = (function(){
       port = new _SocketPort_();
       hasstate = false;
       document.getElementById("loading").style.visibility = "visible";
-      ts.sync();
+      do_sync();
     }
     else
     {
@@ -2020,12 +2033,14 @@ var drawsocket = (function(){
 
     ts = timesync.create({
         server:   port,
-        interval: 5000,
-        delay: 250
+        interval: null,
+        repeat: 5,
+        delay: 100
     });
 
     ts.on('sync', function (state) {
 
+      console.log('syncing', state);
       if( state === 'end' )
       {
         display_log('sync offset: ' + ts.offset + ' ms');
@@ -2037,9 +2052,11 @@ var drawsocket = (function(){
           document.getElementById("loading").style.visibility = "hidden";
 
           // ask server for current state
-        port.sendObj({ statereq: 1 });
-        hasstate = true;
+          port.sendObj({ statereq: 1 });
+
+          hasstate = true;
         }
+
       }
       else
       {
@@ -2067,11 +2084,12 @@ var drawsocket = (function(){
   
 
     ts.send = function (socket, data, timeout) {
+  //    console.log('sending', data, timeout);
       return new Promise(function (resolve, reject) {
         let timeoutFn = setTimeout(reject, timeout);
         if(port.readyState === port.OPEN)
         {
-      //    console.log(data);
+         //console.log('data',data);
           port.sendObj({
             timesync: data
           });
@@ -2091,7 +2109,8 @@ var drawsocket = (function(){
       this.ts.receive(null, data);
     });
     */
-
+    
+    
 
     if (typeof document.addEventListener === "undefined" || hidden === undefined) {
       console.log("Page Visibility API not found");
@@ -2103,6 +2122,8 @@ var drawsocket = (function(){
     initMultitouch("main-div");
     initMultitouch("touchdiv");
 
+    
+    do_sync();
   }
 
 
