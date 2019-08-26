649945472e3affd9f188fd057325131ac00c7b7f
ramagottfried <rama.gottfried@gmail.com>
2019-08-26 23:26:44 +0200

    verovio tests

--- dev/verovio-test/index.js
+++ dev/verovio-test/index.js
@@ -1,15 +1,167 @@
-var verovio = require('verovio-dev');
-var fs = require("fs");
+const verovio = require('verovio-dev');
+const fs = require("fs");
+const xslt = require("xslt-processor");
 
-var vrvToolkit = new verovio.toolkit(512);
+const { Midi } = require('@tonejs/midi');
 
-mei = fs.readFileSync("Beethoven_Sonata_no.29_op.106.mei");
+const midiFile = require('midi-file');
 
-vrvToolkit.loadData(mei.toString());
+ 
+// xmlString: string of xml file contents
+// xsltString: string of xslt file contents
+// outXmlString: output xml string.
 
 
+let vrvToolkit = new verovio.toolkit(512);
 
-svg = vrvToolkit.renderToSVG(1, {});
+vrvToolkit.setOptions({
+    scale: 100,
+    evenNoteSpacing : true
+});
 
 
+let src = fs.readFileSync("Beethoven_Sonata_no.29_op.106.mei");
+//let src = fs.readFileSync("MozartTrio.musicxml");
+
+vrvToolkit.loadData(src.toString());
+/*
+const xmlString = fs.readFileSync("MozartTrio.musicxml");
+const xsltString = fs.readFileSync("musicxml2mei-3.0.xsl");
+
+const src = xslt.xsltProcess(
+    xslt.xmlParse(xmlString.toString()),
+    xslt.xmlParse(xsltString.toString())
+);
+fs.writeFileSync("testXML", src);
+*/
+
+//vrvToolkit.loadData(mei);
+
+//vrvToolkit.loadData(src.toString());
+
+/*
+let mei = `<mei xmlns="http://www.music-encoding.org/ns/mei">
+<meiHead>
+    <fileDesc>
+        <titleStmt>
+            <title/>
+        </titleStmt>
+        <pubStmt/>
+    </fileDesc>
+</meiHead>
+<music>
+    <body>
+        <mdiv>
+            <score>
+                <scoreDef>
+                    <staffGrp>
+                        <staffDef clef.shape="G" clef.line="2" n="1" lines="5"/>
+                    </staffGrp>
+                </scoreDef>
+                <section>
+                    <measure>
+                        <staff n="1">
+                            <layer>
+                                <note pname="c" oct="4" dur="4"/>
+                                <note pname="c" oct="4" dur="8"/>
+                                <note pname="c" oct="4" dur="16"/>
+                                <note pname="c" oct="4" dur="4"/>
+
+                            </layer>
+                        </staff>
+                    </measure>
+                </section>
+            </score>
+        </mdiv>
+    </body>
+</music>
+</mei>`;
+
+*/
+
+let svg = vrvToolkit.renderToSVG(1, {});
 fs.writeFileSync("hello.svg", svg);
+
+fs.writeFileSync("generatedMEI.mei", vrvToolkit.getMEI() );
+
+var buf = Buffer.from( vrvToolkit.renderToMidi(), 'base64'); 
+fs.writeFileSync("generatedMIDI.midi", buf );
+
+// const BASE64_MARKER = ';base64,';
+// function convertDataURIToBinary(dataURI) {
+//     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+//     var base64 = dataURI.substring(base64Index);
+//     var raw = Buffer.from( vrvToolkit.renderToMidi(), 'base64');// window.atob(base64);
+//     var rawLength = raw.length;
+//     var array = new Uint8Array(new ArrayBuffer(rawLength));
+    
+//     for(let i = 0; i < rawLength; i++) {
+//         array[i] = raw.charCodeAt(i);
+//     }
+
+//     return array;
+// }
+
+
+function MIDI64toArrayBuffer(midi_64)
+{
+    const raw = Buffer.from( midi_64, 'base64');// window.atob(base64);
+    const rawLength = raw.length;
+    let array = new ArrayBuffer(rawLength);
+    
+    for(let i = 0; i < rawLength; i++) {
+        array[i] = raw[i];
+    }
+
+    return array;
+}
+
+function toArrayBuffer(buf) {
+    var ab = new ArrayBuffer(buf.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buf.length; ++i) {
+        view[i] = buf[i];
+    }
+    return ab;
+}
+
+var byteArray = toArrayBuffer( buf );
+
+
+
+const midi = new Midi( byteArray );
+
+//console.log(typeof midi);
+
+let parsedFile = midiFile.parseMidi(buf);
+
+fs.writeFileSync('parsedMIDI.json', JSON.stringify(midi) );
+
+
+//the file name decoded from the first track
+const name = midi.name
+//get the tracks
+midi.tracks.forEach(track => {
+  //tracks have notes and controlChanges
+
+  //notes are an array
+  const notes = track.notes
+  notes.forEach((note, i) => {
+      console.log(i, note);
+      
+    //note.midi, note.time, note.duration, note.name
+  })
+
+  /*
+  //the control changes are an object
+  //the keys are the CC number
+  track.controlChanges[64]
+  //they are also aliased to the CC number's common name (if it has one)
+  track.controlChanges.sustain.forEach(cc => {
+    // cc.ticks, cc.value, cc.time
+  })
+
+  //the track also has a channel and instrument
+  //track.instrument.name
+  */
+})
